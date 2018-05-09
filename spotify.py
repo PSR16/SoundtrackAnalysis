08a0848f13f1c656170eb72e4c3e6eef9f70f61c
Pegah Rashidnia <PRashidnia@gmail.com>
2018-05-09 17:50:01 -0400

    refactored

--- spotify.py
+++ spotify.py
@@ -34,7 +34,8 @@ def get_tracks(db, album_url, sp):
         track_uri = track["uri"]
         print(track_name)
         print(track_uri)
-        get_features(track_uri, sp)
+    #    get_features(track_uri, sp)
+        get_sections(track_uri, sp)
 
 
 def get_features(track_uri, sp):
@@ -64,10 +65,50 @@ def get_features(track_uri, sp):
     df2.drop(columns=['pitches'],inplace=True)
     df3 = df2.T
     new_header = df3.iloc[0] #grab the first row for the header
-    df3= df3[1:] #take the data less the header row
+    df3 = df3[1:] #take the data less the header row
     df3.columns = new_header #set the header row as the df header
-    
+
     json_segments = df3.to_json(orient="split")
+#    print(df3)
+    #visualize(df3)
+#    exit(0)
+
+def get_sections(track_uri, sp):
+        """
+        Get detailed features for each track_uri
+
+        :param track_uri: URI for the track
+        """
+
+        features = sp.audio_features(track_uri)
+
+        analysis = sp._get(features[0]['analysis_url'])
+        segments = analysis['segments']
+
+        new_segment = []
+        for segment in segments:
+            start = segment["start"]
+            loud_time = segment["loudness_max_time"]
+            time = start + loud_time
+        #    pitches = segment["pitches"]
+            new_segment.append({"time": time, "pitches": pitches})
+
+        json1 = json.dumps(new_segment, indent=4)
+
+        df = pd.read_json(json1, orient='records')
+        df1 = pd.DataFrame(df['pitches'].values.tolist(), index=df.index)
+
+        df2 = pd.concat([df, df1], axis=1)
+        df2.drop(columns=['pitches'],inplace=True)
+        df3 = df2.T
+        new_header = df3.iloc[0] #grab the first row for the header
+        df3 = df3[1:] #take the data less the header row
+        df3.columns = new_header #set the header row as the df header
+
+        json_segments = df3.to_json(orient="split")
+    #    print(df3)
+        #visualize(df3)
+    #    exit(0)
 
 def visualize(df):
     """
@@ -76,7 +117,7 @@ def visualize(df):
     :param df: pandas dataframe
     """
     ax = plt.axes()
-    ax = sns.heatmap(df, ax = ax)
+    ax = sns.heatmap(df, ax = ax, linewidths=0.1, cmap="Set3")
     plt.show()
 
 def main():
